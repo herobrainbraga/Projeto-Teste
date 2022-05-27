a09388a1484a1fd7e9c494a9c555875d6bd592e0
herobrainbraga <hero.b.braga@gmail.com>
2022-05-27 16:30:13 -0300

    Adicionar arquivos de projeto.

---
+++ Recursivo/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+
+namespace Recursivo
+{
+    public class Recursive
+    {
+        public void Recursivao()
+        {
+
+            int x = 1;
+
+            do
+            {
+                int.TryParse(Console.ReadLine(), out x);
+
+                if (x == 2)
+                {
+                    Console.WriteLine("Para de escrever 2, isso machuca");
+                }
+
+            }
+            while (x != 0);
+
+            Console.WriteLine("Parabéns!");
+
+
+        }
+    }
+}
---
+++ Recursivo/chamada de outro arquivo.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Recursivo
+{
+    public class chamada_de_outro_arquivo
+    {
+
+        public static void NaoMain()
+        {
+
+            Recursive recursivo = new Recursive();
+
+            recursivo.Recursivao();
+
+            Console.WriteLine("Caralho eu to usando dois arquivos que foda");
+
+        }
+
+    }
+}
